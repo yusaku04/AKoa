6fcbfa58bf590beb5dbc9f838fdcafab02468f23
yusaku04 <43626971+yusaku04@users.noreply.github.com>
2019-07-28 09:14:21 +0800

    Add files via upload

--- AutoUbuntu.sh
+++ AutoUbuntu.sh
@@ -1,25 +1,39 @@
 
 # extract ip address
+echo \> GET IPADRESS
 IPADDRESS=$(wget -qO- ipv4.icanhazip.com);
 IPADD="s/ipaddresxxx/$IPADDRESS/g";
-
+sleep 1
 # set time GMT +8
 echo \> Changing Server Time Zone...
 ln -fs /usr/share/zoneinfo/Asia/Manila /etc/localtime
-
+sleep 1
 # System Update
+echo /> Sytem Update
 apt-get update
 apt-get upgrade
+sleep 1
 # Install Openvpn
+echo /> Installing OPENVPN
 apt-get -y install openvpn 
+sleep 1
 # Install Firewall
+echo /> Installing Firewall
 apt-get -y install ufw
+sleep 1
 #Installing Easy-RSA...
+echo /> Installing RSA
 apt-get -y install easy-rsa
-Installing Apache2 Web Server...
+sleep 1
+
+#Installing Apache2 Web Server...
+echo /> Installing Apache2
 apt-get -y install apache2
+sleep 1
 #Install Squid
+echo /> Installing Squid
 apt-get -y install squid
+sleep 1
 #Install Zip
 apt-get -y install zip
 #Install Privoxy
